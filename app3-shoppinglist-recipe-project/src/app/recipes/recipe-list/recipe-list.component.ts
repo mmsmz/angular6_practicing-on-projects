de90c78ad2910a6f39178ebad643c54bc7579b72
style <marafath07@gmail.com>
2020-07-13 17:33:21 +0530

    added the componntes are amended the issues related to bootstraps

---
+++ app3-shoppinglist-recipe-project/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+import {Recipe} from '../recipe.model';
+
+@Component({
+  selector: 'app-recipe-list',
+  templateUrl: './recipe-list.component.html',
+  styleUrls: ['./recipe-list.component.css']
+})
+export class RecipeListComponent implements OnInit {
+
+  recipes: Recipe[] = [
+    new Recipe('A Test Recipe', 'This is simply the desciption', 'https://i.ytimg.com/vi/Vv5Iq-kU1Y4/maxresdefault.jpg'),
+    new Recipe('A Test Recipe', 'This is simply the desciption', 'https://i.ytimg.com/vi/Vv5Iq-kU1Y4/maxresdefault.jpg'),
+    new Recipe('A Test Recipe', 'This is simply the desciption', 'https://i.ytimg.com/vi/Vv5Iq-kU1Y4/maxresdefault.jpg')
+  ];
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
